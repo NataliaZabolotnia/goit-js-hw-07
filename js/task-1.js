a3b9a66a5d3576769aa41913c72b3cd68104d6f0
Natalia Zabolotnia <zanatav78@gmail.com>
2025-05-18 20:30:01 +0300

    task-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,22 +1,12 @@
-const categoriesList=document.querySelector("#categories");
-const items=categoriesList.querySelectorAll(".item");
+const categoriesList = document.querySelector("#categories");
+const items = categoriesList.querySelectorAll(".item");
 
 console.log(`Number of categories:${items.length}`);
 
-items.forEach(item=>{
-const title=item.querySelector("h2").textContent;
-const elementCount=item.querySelectorAll("ul li");
+items.forEach((item) => {
+  const title = item.querySelector("h2").textContent;
+  const elementCount = item.querySelectorAll("ul li");
 
-console.log(`Category:${title}`);
-console.log(`Elements:${elementCount.length}`);
-});
-images.forEach(image=>{
-const img=document.createElement("img");
-const li=document.createElement("li");
-
-img.src=image.url;
-img.alt=image.alt;
-
-li.appendChild(img);
-imageList.appendChild(li);
+  console.log(`Category:${title}`);
+  console.log(`Elements:${elementCount.length}`);
 });
